ae1df8e47cc2e3d2c25337d748fd644d04fa11da
user <romka1999@inbox.lv>
2018-10-22 11:39:41 +0300

    20181022_11_39

---
+++ test20181022.py
@@ -0,0 +1,416 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+
+Traceback (most recent call last):
+  File "/home/user/Python test.py", line 1, in <module>
+    if x < 2 :
+NameError: name 'x' is not defined
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+
+Traceback (most recent call last):
+  File "/home/user/Python test.py", line 1, in <module>
+    if x< 2 :
+NameError: name 'x' is not defined
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+
+Traceback (most recent call last):
+  File "/home/user/Python test.py", line 26, in <module>
+    w
+NameError: name 'w' is not defined
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+-2.5
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+-2.5
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+-2.5
+Hello
+Yo
+7
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+-2.5
+Hello
+Yo
+I'm lumberjack, and Im okay.
+I sleep all night and work all day.
+7
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+-2.5
+Hello
+Yo
+I'm lumberjack, and Im okay.
+I sleep all night and work all day.
+7
+8
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+-2.5
+Hello
+Yo
+I'm lumberjack, and Im okay.
+I sleep all night and work all day.
+7
+8
+5
+4
+3
+2
+1
+Blastoff!
+0
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+small
+All done
+Hello
+('Done', -1)
+Hello
+Fun
+Zip
+Hello
+Fun
+w
+ 
+0.99
+42.0
+-2.5
+Hello
+Yo
+I'm lumberjack, and Im okay.
+I sleep all night and work all day.
+7
+8
+5
+4
+3
+2
+1
+Blastoff!
+0
+Dry off!
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+>
+===================== RESTART: /home/user/Python test.py =====================
+('Happy New Year:', 'JosephGlenn')
+('Happy New Year:', 'Sally')
+Done!
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+('Happy New Year:', 'Joseph')
+('Happy New Year:', 'Glenn')
+('Happy New Year:', 'Sally')
+Done!
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+('Happy New Year:', 'Joseph')
+('Happy New Year:', 'Glenn')
+('Happy New Year:', 'Sally')
+Done!
+5
+4
+3
+2
+1
+Blastoff!
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+('Happy New Year:', 'Joseph')
+('Happy New Year:', 'Glenn')
+('Happy New Year:', 'Sally')
+Done!
+5
+4
+3
+2
+1
+Blastoff!
+5
+4
+3
+2
+1
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+('Happy New Year:', 'Joseph')
+('Happy New Year:', 'Glenn')
+('Happy New Year:', 'Sally')
+Done!
+5
+4
+3
+2
+1
+Blastoff!
+5
+4
+3
+2
+1
+Before
+9
+41
+12
+3
+74
+15
+After
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+('Happy New Year:', 'Joseph')
+('Happy New Year:', 'Glenn')
+('Happy New Year:', 'Sally')
+Done!
+5
+4
+3
+2
+1
+Blastoff!
+5
+4
+3
+2
+1
+Before
+9
+41
+12
+3
+74
+15
+After
+('Before', -1)
+(9, 9)
+(41, 41)
+(41, 12)
+(41, 3)
+(74, 74)
+(74, 15)
+('After', 74)
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+('Happy New Year:', 'Joseph')
+('Happy New Year:', 'Glenn')
+('Happy New Year:', 'Sally')
+Done!
+5
+4
+3
+2
+1
+Blastoff!
+5
+4
+3
+2
+1
+Before
+9
+41
+12
+3
+74
+15
+After
+('Before', -1)
+(9, 9)
+(41, 41)
+(41, 12)
+(41, 3)
+(74, 74)
+(74, 15)
+('After', 74)
+('Before', 0)
+(1, 9)
+(2, 41)
+(3, 12)
+(4, 3)
+(5, 74)
+(6, 15)
+('After', 6)
+>>> 
+===================== RESTART: /home/user/Python test.py =====================
+('Before', 0)
+(9, 9)
+(50, 41)
+(62, 12)
+(65, 3)
+(139, 74)
+(154, 15)
+('After', 154)
+>>> 
